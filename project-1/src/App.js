69bec86871af64a54aa68b0a592810441f48bd20
sakatiharikrishna <sakatiharikrishna@gmail.com>
2023-08-25 14:05:22 +0530

    first-commit

---
+++ project-1/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hii Iam Sakati Hari Krishna </p>
+    </div>
+  );
+}
+
+export default App;
